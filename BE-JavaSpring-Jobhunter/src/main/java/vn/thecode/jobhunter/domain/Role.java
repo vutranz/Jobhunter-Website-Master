96b20ca24875df8e54cc4b7c460af9e0dc4f8bb1
vu dep trai <vu@gmail.com>
2025-05-03 05:50:02 +0700

    update role and user

--- BE-JavaSpring-Jobhunter/src/main/java/vn/thecode/jobhunter/domain/Role.java
+++ BE-JavaSpring-Jobhunter/src/main/java/vn/thecode/jobhunter/domain/Role.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -44,6 +45,10 @@ public class Role {
     @JsonIgnoreProperties(value = { "roles" })
     @JoinTable(name = "permission_role", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
     private List<Permission> permissions;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    List<User> users;
 
     @PrePersist
     public void handleBeforeCreateAt() {
--- BE-JavaSpring-Jobhunter/src/main/java/vn/thecode/jobhunter/domain/User.java
+++ BE-JavaSpring-Jobhunter/src/main/java/vn/thecode/jobhunter/domain/User.java
@@ -50,6 +50,10 @@ public class User {
     @JsonIgnore
     List<Resume> resumes;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
     @PrePersist
     public void handleBeforeCreateAt() {
         this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
